6192d681db9c2e8049b59b3ed46caac65cc4a71e
unknown <k.maczkowska@gmail.com>
2017-02-28 18:17:41 +0100

    initial commit

---
+++ src/test/java/com/kasia/Application.java
@@ -0,0 +1,10 @@
+package com.kasia;
+
+/**
+ * Created by RENT on 2017-02-28.
+ */
+public class Application {
+    public static void main(String[] args) {
+        System.out.println(4);
+    }
+}
